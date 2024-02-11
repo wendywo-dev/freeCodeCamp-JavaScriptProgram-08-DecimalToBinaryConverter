8b789eddc320b2d13f269005460ed7451d9e75ee
wendywo-dev <142320911+wendywo-dev@users.noreply.github.com>
2024-02-11 22:27:28 +0800

    upload all files

---
+++ README.md
@@ -0,0 +1,5 @@
+# freeCodeCamp JavaScript Algorithms and Data Structures (Beta) course
+
+## 08-Decimal To Binary Converter
+
+This project is coded by following a step by step tutorial which is part of the [JavaScript Algorithms and Data Structures (Beta) course](https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures-v8/) created by freeCodeCamp.
---
+++ script.js
@@ -0,0 +1,65 @@
+const numberInput = document.getElementById("number-input");
+const convertBtn = document.getElementById("convert-btn");
+const result = document.getElementById("result");
+
+const decimalToBinary = (input) => {
+  const inputs = [];
+  const quotients = [];
+  const remainders = [];
+
+  if (input === 0) {
+    result.innerHTML = "0";
+    return;
+  }
+
+  while (input > 0) {
+    const quotient = Math.floor(input / 2);
+    const remainder = input % 2;
+
+    inputs.push(input);
+    quotients.push(quotient);
+    remainders.push(remainder);
+    input = quotient;
+  }
+
+  console.log("Inputs: ", inputs);
+  console.log("Quotients: ", quotients);
+  console.log("Remainders: ", remainders);
+
+  result.innerText = remainders.reverse().join("");
+};
+
+/* a more efficient version
+const decimalToBinary = (input) => {
+  let binary = "";
+
+  if (input === 0) {
+    binary = "0";
+  }
+
+  while (input > 0) {
+    binary = (input % 2) + binary;
+    input = Math.floor(input / 2);
+  }
+
+  result.innerText = binary;
+};
+*/
+
+const checkUserInput = () => {
+  if (!numberInput.value || isNaN(parseInt(numberInput.value))) {
+    alert("Please provide a decimal number");
+    return;
+  }
+
+  decimalToBinary(parseInt(numberInput.value));
+  numberInput.value = "";
+};
+
+convertBtn.addEventListener("click", checkUserInput);
+
+numberInput.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    checkUserInput();
+  }
+});
